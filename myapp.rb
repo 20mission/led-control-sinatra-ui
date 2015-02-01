fd761bd5c42c4a02d0a5454d77f1a8829301a771
Chris C <glitch0@gmail.com>
2015-02-01 14:03:05 -0800

    Added rails support library for underscore method

--- myapp.rb
+++ myapp.rb
@@ -1,14 +1,21 @@
 # myapp.rb
 require 'sinatra'
+require 'active_support/all'
 
 set :bind, '0.0.0.0'
 set :port, 2974
 
 get '/' do
-  'Hello!'
+  patterns = ["Volume", "LightsOff", "LightsOn", "MicrophonePulse", "FastMicrophonePulse", "Ants", "Spin", "Rainbow", "RainbowCandy", "Fade", "CrazyColorStrobe", "ColorStrobe", "Strobe", "TestPixel"] 
+  result = "<html><body>"
+  patterns.each do |p|
+    result += "<div><a href='/newMode/#{p.underscore}'><button>#{p}</button></a></div>"
+  end
+  result += "</body></html>"
+  result
 end
 
-get '/:newMode' do
+get '/newMode/:newMode' do
   s = TCPSocket.open("127.0.0.1", "2973")
   to_send = "set pattern #{params[:newMode]}"
   puts to_send
@@ -16,3 +23,4 @@ get '/:newMode' do
   s.close
   "Changed mode to #{params[:newMode]}"
 end
+
